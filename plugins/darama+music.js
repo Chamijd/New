f95e8aa5e0b39a5dec17a9a4b00bf57df963dbf6
Chamijd <chamindumax28@gmail.com>
2025-03-20 14:20:35 +0530

    Update darama+music.js

--- plugins/darama+music.js
+++ plugins/darama+music.js
@@ -1,97 +1,122 @@
-const { cmd, commands } = require('../command');
-const fg = require('api-dylux');
-const yts = require('yt-search');
-
+// Audio download command
 cmd({
-    pattern: "download",
-    desc: "To download song or video.",
-    react: "🎶",
+    pattern: "so",
+    desc: "To download songs.",
+    react: "🎵",
     category: "download",
     filename: __filename
 }, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-        // If user replied to the previous message to select type of download
-        if (!q) {
-            return reply(
-                "Please reply with the option for download.\n\nFor song: `!song <song_name>` \nFor video: `!video <video_name>`."
-            );
-        }
-        
-        if (command === 'song') {
-            // Process song download
-            const search = await yts(q);
-            const data = search.videos[0];
-            const url = data.url;
-            
-            let desc = `
+        if (!q) return reply("Please provide a title or URL of the song.");
+
+        // Search for the song
+        const search = await yts(q);
+        const data = search.videos[0];
+        const url = data.url;
+
+        // Send initial message
+        let desc = `
 *⫷⦁DEVIL-TECH-MD MUSⵊC DOWNLOADⵊNG⦁⫸*
 
-🎵 *MUSⵊC FOUND!* 
+🎵 *MUSⵊC FOUND!*
 
-➥ *Title:* ${data.title} 
-➥ *Duration:* ${data.timestamp} 
-➥ *Views:* ${data.views} 
-➥ *Uploaded On:* ${data.ago} 
-➥ *Link:* ${data.url} 
+➥ *Title:* ${data.title}
+➥ *Duration:* ${data.timestamp}
+➥ *Views:* ${data.views}
+➥ *Uploaded On:* ${data.ago}
+➥ *Link:* ${data.url}
 
-🎧 *ENJOY THE MUSIC BROUGHT TO YOU!*
+🎧 *ENJOY THE MUSIC!*
 
-> *DEVIL-TECH-MD WHATSAPP BOT* 
-
-> *© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴄʏʙᴇʀ ʟᴏᴋᴜ ᴀꜱʜᴜᴜ ᴏꜟ* 
+> *DEVIL-TECH-MD WHATSAPP BOT*
+> *© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴄʏʙᴇʀ ʟᴏᴋᴜ ᴀꜱʜᴜᴜ ᴏꜟ*
 `;
-            await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
 
-            // Download audio
-            let down = await fg.yta(url);
-            if (!down || !down.dl_url) {
-                return reply(`_Error: Unable to fetch the download link. Try again later._`);
-            }
+        // Send thumbnail and description of the song
+        await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
 
+        // Download song audio
+        let down = await fg.yta(url);
+        
+        // Log the entire response
+        console.log("Download response:", down);
+
+        if (down && down.dl_url) {
             let downloadUrl = down.dl_url;
 
             // Send audio message
             await conn.sendMessage(from, { audio: { url: downloadUrl }, mimetype: "audio/mpeg" }, { quoted: mek });
+
+            // Send audio as document
             await conn.sendMessage(from, { document: { url: downloadUrl }, mimetype: "audio/mpeg", fileName: data.title + ".mp3", caption: "*© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴄʏʙᴇʀ ʟᴏᴋᴜ ᴀꜱʜᴜᴜ ᴏꜟ*" }, { quoted: mek });
-        } else if (command === 'video') {
-            // Process video download
-            const search = await yts(q);
-            const data = search.videos[0];
-            const url = data.url;
-            
-            let desc = `
+        } else {
+            reply(`_Hi ${pushname}, no valid download link found. Please try again._`);
+        }
+
+    } catch (e) {
+        console.log(e);
+        reply(`_Hi ${pushname}, please try again later._`);
+    }
+});
+
+// Video download command
+cmd({
+    pattern: "video",
+    alias: ["v"],
+    desc: "To download videos.",
+    react: "🎥",
+    category: "download",
+    filename: __filename
+}, async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!q) return reply("Please provide a title or URL of the video.");
+
+        // Search for the video
+        const search = await yts(q);
+        const data = search.videos[0];
+        const url = data.url;
+
+        // Send initial message
+        let desc = `
 *⫷⦁DEVIL-TECH-MD VⵊDEO DOWNLOADⵊNG⦁⫸*
 
-🎥 *VⵊDEO FOUND!* 
+🎥 *VⵊDEO FOUND!*
 
-➥ *Title:* ${data.title} 
-➥ *Duration:* ${data.timestamp} 
-➥ *Views:* ${data.views} 
-➥ *Uploaded On:* ${data.ago} 
-➥ *Link:* ${data.url} 
+➥ *Title:* ${data.title}
+➥ *Duration:* ${data.timestamp}
+➥ *Views:* ${data.views}
+➥ *Uploaded On:* ${data.ago}
+➥ *Link:* ${data.url}
 
-🎬 *ENJOY THE VIDEO BROUGHT TO YOU!*
+🎬 *ENJOY THE VIDEO!*
 
-> *DEVIL-TECH-MD WHATSAPP BOT* 
-
+> *DEVIL-TECH-MD WHATSAPP BOT*
 > *© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴄʏʙᴇʀ ʟᴏᴋᴜ ᴀꜱʜᴜᴜ ᴏꜟ*
 `;
-            await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
 
-            // Download video
-            let down = await fg.ytv(url);
-            if (!down || !down.dl_url) {
-                return reply(`_Error: Unable to fetch the download link. Try again later._`);
-            }
+        // Send thumbnail and description of the video
+        await conn.sendMessage(from, { image: { url: data.thumbnail }, caption: desc }, { quoted: mek });
 
+        // Download video
+        let down = await fg.ytv(url);
+        
+        // Log the entire response
+        console.log("Download response:", down);
+
+        if (down && down.dl_url) {
             let downloadUrl = down.dl_url;
 
             // Send video message
             await conn.sendMessage(from, { video: { url: downloadUrl }, mimetype: "video/mp4" }, { quoted: mek });
+
+            // Send video as document
             await conn.sendMessage(from, { document: { url: downloadUrl }, mimetype: "video/mp4", fileName: data.title + ".mp4", caption: "*© ᴄʀᴇᴀᴛᴇᴅ ʙʏ ᴄʏʙᴇʀ ʟᴏᴋᴜ ᴀꜱʜᴜᴜ ᴏꜟ*" }, { quoted: mek });
+        } else {
+            reply(`_Hi ${pushname}, no valid download link found. Please try again._`);
         }
+
     } catch (e) {
         console.log(e);
-        reply(`_Hi ${pushname}, retry later_`);
+        reply(`_Hi ${pushname}, please try again later._`);
     }
 });
