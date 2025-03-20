4637f6f0151bc277b9e67e6470c22e30bfe8f833
Chamijd <chamindumax28@gmail.com>
2025-03-20 06:13:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const port = process.env.PORT || 9090;
   console.log('Plugins installed successful ✅')
   console.log('Bot connected to whatsapp ✅')
   
-  let up = `*Hello there DEVIL-TECH-MD User! \ud83d\udc4b\ud83c\udffb* \n\n> Simple , Straight Forward But Loaded With Features \ud83c\udf8a, Meet CHAMA-MD WhatsApp Bot.\n\n *Thanks for using CHAMA-MD \ud83d\udea9* \n\n> Join WhatsApp Channel :- ⤵️\n \nhttps://whatsapp.com/channel/0029Vb9WF4nJJhzeUCFS6M0u\n\n- *YOUR PREFIX:* = ${prefix}\n\nDont forget to give star to repo ⬇️\n\nhttps://github.com\n\n> © Powered BY ᴄʜᴀ〽️ɪɴᴅᴜ \ud83d\udda4`;
+  let up = `*Hello there CHAMA 〽️D User! \ud83d\udc4b\ud83c\udffb* \n\n> Simple , Straight Forward But Loaded With Features \ud83c\udf8a, Meet CHAMA-MD WhatsApp Bot.\n\n *Thanks for using CHAMA-MD \ud83d\udea9* \n\n> Join WhatsApp Channel :- ⤵️\n \nhttps://whatsapp.com/channel/0029Vb9WF4nJJhzeUCFS6M0u\n\n- *YOUR PREFIX:* = ${prefix}\n\nDont forget to give star to repo ⬇️\n\nhttps://github.com\n\n> © Powered BY JawadTechX \ud83d\udda4`;
     conn.sendMessage(conn.user.id, { image: { url: `https://i.ibb.co/KxYGBGdf/8947.jpg` }, caption: up })
   }
   })
